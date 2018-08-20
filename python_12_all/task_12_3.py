86b18711787d9a4ef5f4e0e109e40faa4d6a170a
fear <alex-bolotin88@yandex.ru>
2018-08-20 10:31:32 +0300

    tack 12 done

--- python_12_all/task_12_3.py
+++ python_12_all/task_12_3.py
@@ -21,3 +21,15 @@ Reachable    Unreachable
 То есть, до выполнения функции и после списки должны выглядеть одинаково.
 
 '''
+
+from tabulate import tabulate
+from itertools import zip_longest
+
+def ip_table(reachable,unreachable):
+    print(tabulate(zip_longest(reachable,unreachable),headers=['Reachable','Unreachable']))
+
+
+if __name__=='__main__':
+    reachable = ['10.1.1.1','10.1.1.2']
+    unreachable = ['10.1.1.7','10.1.1.8','10.1.1.9']
+    ip_table(reachable,unreachable)    
