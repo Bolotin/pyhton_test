fc6d17ce8cd292c7ef275f638778ead229e4b801
Fear <alex-bolotin88@yandex.ru>
2020-02-15 14:48:43 +0300

    new try

--- exercises/07_files/07_02.py
+++
@@ -1,7 +0,0 @@
-from sys import argv
-
-file_name = argv[1]
-with open(file_name,'r') as file:
-    for line in file:
-        if not line.startswith('!'):
-            print(line.rstrip())
--- exercises/15_module_re/15_01.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-from re import search
-
-filename, regex = argv[1:]
-
-with open(filename,'r') as file:
-    for line in file:
-        if search(regex, line):
-            print(line.strip())
